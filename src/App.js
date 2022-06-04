bcafcf9be59c6113c7836b1f20bd71883981dce2
NMShihab <nmshihabislam@gmail.com>
2022-06-04 11:40:55 +0600

    add route

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Login from "./Pages/Login";
+import Registration from "./Pages/Registration";
+import Home from "./Pages/Home";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="registration" element={<Registration />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  Collapse,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarText,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar color="light" expand="md" light>
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={function noRefCheck() {}} />
+        <Collapse navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                GitHub
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
