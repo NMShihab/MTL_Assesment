555009e263faf4c366646f003271ee463ad0e815
NMShihab <nmshihabislam@gmail.com>
2022-06-04 12:50:40 +0600

    add registration form and functionality

--- src/Pages/Registration.js
+++ src/Pages/Registration.js
@@ -1,7 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Button,
+  CardFooter,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Registration = () => {
-  return <div>Registration</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isDisable, setIsDisable] = useState(true);
+
+  const confirmPasswordHandler = (e) => {
+    setConfirmPassword(e.target.value);
+    if (password === e.target.value && password.length >= 8) {
+      setIsDisable(false);
+    }
+  };
+
+  const handleRegistration = async (e) => {
+    e.preventDefault();
+    console.log({ email, password, confirmPassword });
+    const data = { email, password };
+    try {
+      const response = await axios.post("http://localhost:4000/register", data);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="d-flex align-items-center justify-content-center">
+      <Card className="m-5 w-50 shadow-sm p-3 mb-5 bg-white rounded">
+        <CardHeader>Registration</CardHeader>
+        <CardBody>
+          <Form onSubmit={handleRegistration}>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                placeholder="Enter email"
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input
+                id="password"
+                name="password"
+                placeholder="Enter password"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <ul>
+                <li>Password Must be 8 charecters long</li>
+              </ul>
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Confirm Password</Label>
+              <Input
+                id="confirm-password"
+                name="password"
+                placeholder="Confirm Password"
+                type="password"
+                onChange={confirmPasswordHandler}
+              />
+            </FormGroup>
+            <Button color="primary" disabled={isDisable}>
+              Register
+            </Button>
+          </Form>
+        </CardBody>
+        <CardFooter>
+          If You already registered then <Link to="/login"> Login here</Link>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
 
 export default Registration;
